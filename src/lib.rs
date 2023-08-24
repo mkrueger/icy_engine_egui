cbf84a25a00ae6b99000bdfd3edc6e01f8103048
Mike Krüger <mkrueger@posteo.de>
2023-08-24 09:07:05 +0200

    Initial import.

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+pub mod ui;
+pub use ui::*;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct MonitorSettings {
+    pub use_filter: bool,
+
+    pub monitor_type: usize,
+
+    pub gamma: f32,
+    pub contrast: f32,
+    pub saturation: f32,
+    pub brightness: f32,
+    pub light: f32,
+    pub blur: f32,
+    pub curvature: f32,
+    pub scanlines: f32,
+}
+
+unsafe impl Send for MonitorSettings {}
+unsafe impl Sync for MonitorSettings {}
+
+impl Default for MonitorSettings {
+    fn default() -> Self {
+        Self {
+            use_filter: false,
+            monitor_type: 0,
+            gamma: 50.,
+            contrast: 50.,
+            saturation: 50.,
+            brightness: 30.,
+            light: 40.,
+            blur: 30.,
+            curvature: 10.,
+            scanlines: 10.,
+        }
+    }
+}
---
+++ src/ui/mod.rs
@@ -0,0 +1,8 @@
+pub mod buffer_view;
+pub use buffer_view::*;
+
+pub mod smooth_scroll;
+pub use smooth_scroll::*;
+
+pub mod keymaps;
+pub use keymaps::*;
